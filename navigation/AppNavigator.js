34231ceac1c535c84b7edb7dfc9d45027f35ebdf
rain <753342031@qq.com>
2019-05-23 00:38:47 +0800

    build apk

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { Text } from 'react-native'
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+let a =  class extends React.Component{
+  render(){
+    return <Text>123</Text>
+  }
+};
 
-export default createAppContainer(createSwitchNavigator({
+export default createAppContainer(createStackNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
 }));
+
+/* 
+function () {
+  return (
+    <Text>123</Text>
+  )
+} */
